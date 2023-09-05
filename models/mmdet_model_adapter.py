0d92d7f8485e19ba5ebbada8bcf8783f56c48c92
shenmishajing <shenmishajing@gmail.com>
2023-09-05 10:03:29 +0800

    add rescale mask feature and name feature

--- models/mmdet_model_adapter.py
+++ models/mmdet_model_adapter.py
@@ -13,17 +13,37 @@ class MMDetModelAdapter(MMLabModelAdapter):
     def predict_forward(self, batch, *args, **kwargs):
         predict_result = super().predict_forward(batch, *args, **kwargs)
         for output in predict_result["predict_outputs"]:
-            # rescale gt bboxes
-            assert output.get("scale_factor") is not None
-            output.gt_instances.bboxes /= output.gt_instances.bboxes.new_tensor(
-                output.scale_factor
-            ).repeat((1, 2))
+            if output.get("gt_instances") is not None:
+                # rescale gt bboxes
+                if (
+                    output.get("scale_factor") is not None
+                    and output.gt_instances.get("bboxes") is not None
+                ):
+                    output.gt_instances.bboxes /= output.gt_instances.bboxes.new_tensor(
+                        output.scale_factor
+                    ).repeat((1, 2))
+
+                # rescale gt masks
+                if (
+                    output.get("ori_shape") is not None
+                    and output.get("img_shape") is not None
+                    and output.img_shape != output.ori_shape
+                    and output.gt_instances.get("masks") is not None
+                ):
+                    output.gt_instances.masks = output.gt_instances.masks.resize(
+                        output.ori_shape
+                    )
         return predict_result
 
     def predict_result(self, *args, predict_outputs, output_path, **kwargs):
         for output in predict_outputs:
             # result visualization
             name = os.path.basename(output.img_path)
+            if not self.visualizer_kwargs.get("draw_gt", True):
+                name = f"_{output_path.split('/')[-4]}".join(os.path.splitext(name))
+            elif not self.visualizer_kwargs.get("draw_pred", True):
+                name = "_ground_truth".join(os.path.splitext(name))
+
             self.trainer.datamodule.visualizers["predict"].add_datasample(
                 name,
                 mmcv.imread(output.img_path, channel_order="rgb"),
